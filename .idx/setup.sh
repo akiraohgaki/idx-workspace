b48cb9115f33424f3910a3b1ee2f0d5ec2cc6c64
Akira Ohgaki <akiraohgaki@gmail.com>
2024-12-24 05:23:25 +0000

    Add setup.sh

---
+++ .idx/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -f "${HOME}/setup-done" ]; then
+  exit 0
+fi
+
+CONFIGS_BASE_URL='https://raw.githubusercontent.com/akiraohgaki/configs/main'
+ENV_FILE="${HOME}/.localenv"
+
+touch "${ENV_FILE}"
+
+ssh-keygen -t ed25519 -N '' -f "${HOME}/.ssh/id_ed25519"
+ssh-keygen -t rsa -N '' -f "${HOME}/.ssh/id_rsa"
+curl -fsSL -o "${HOME}/.ssh/config" "${CONFIGS_BASE_URL}/ssh/config"
+
+curl -fsSL -o "${HOME}/.gitconfig" "${CONFIGS_BASE_URL}/git/.gitconfig"
+curl -fsSL -o "${HOME}/.gitignore" "${CONFIGS_BASE_URL}/git/.gitignore"
+
+curl -fsSL -o "${HOME}/.editorconfig" "${CONFIGS_BASE_URL}/editorconfig/.editorconfig"
+
+curl -fsSL -o "${HOME}/.codeoss-cloudworkstations/data/Machine/settings.json" --create-dirs "${CONFIGS_BASE_URL}/vscode/settings.json"
+
+echo $'\n'"source ${ENV_FILE}" >>"${HOME}/.bashrc"
+
+rm -rf "$(pwd)/.git"
+
+touch "${HOME}/setup-done"
